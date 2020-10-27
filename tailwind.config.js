dfdf7f2e73af755cabb9de821de76150b685fcce
johnclendvoy <john.c.lendvoy@gmail.com>
2020-10-26 22:08:12 -0600

    Initial project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      'scratch-1': ['Caveat', 'sans-serif'],
+      'scratch-2': ['Gloria Hallelujah', 'sans-serif'],
+    },
+    extend: {
+        rotate: {
+            '-2': '-2deg',
+            '2': '2deg',
+            '-3': '-3deg',
+            '3': '3deg'
+        }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
